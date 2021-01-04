a1d764eefcd99ed98a18200c2201c63b396a87fa
koishi755 <46124641+koishi755@users.noreply.github.com>
2021-01-05 05:49:23 +0900

    Create register.js

---
+++ register.js
@@ -0,0 +1,33 @@
+"use strict";
+
+$('.post').on('click', function(){
+
+    //API Gatewayのエンドポイント
+    var apiUrl = 'https://e510a0t1eb.execute-api.ap-northeast-1.amazonaws.com/dev';
+
+    //ヘッダー
+    var myHeaders = new Headers();
+        // add content type header to object
+        myHeaders.append("Content-Type", "application/json");
+
+    //渡すデータ
+    var raw = JSON.stringify({
+        "user_name": $("#user_name").val(),
+        "password": $("#password").val(),
+        "email": $("#email").val()
+    });
+
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    // make API call with parameters and use promises to get response
+    fetch(apiUrl, requestOptions)
+    .then(response => response.text())
+    .then(result => alert(JSON.parse(result).body))
+    .catch(error => console.log('error', error));
+});
